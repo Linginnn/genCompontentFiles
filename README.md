a2bfc6cd9c148a516c465edf7885f50fcfb9511d
孙宇林 <mananan@care.com>
2021-10-20 14:56:17 +0800

    添加校验规则和注释生成

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # gen-compontent-file README
 
-### 0.0.1 可以用右键生成模版库了
+### 1.0.0 
+可以用右键生成模版库了
+
+#### 开发方法
+
+安装依赖
+``` 
+yarn
+```
+
+* F5进行调试
+
+####打包
+* 全局下载 npm install -g vsce
+* 项目打包 vsce package
+
+
+
+
--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as util from "./util";
-import * as path from "path";
+import { Position } from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  const genCompontent = vscode.commands.registerCommand(
     "gen-compontent-file.genCompontent",
     (uri) => {
       vscode.window
         .showInputBox({
           placeHolder: "请输入组件名", // 在输入框内的提示信息
           prompt: "输入组件名字，首字母大写", // 在输入框下方的提示信息
-          // validateInput: (text) => {
-          //   if (/\b([a-z])/.test(text)) {
-          //     return;
-          //   }
-          //   return "输入组件名字，首字母大写";
-          // },
+          validateInput: (text) => {
+            if (/^[A-Z][A-z0-9]*$/.test(text)) {
+              return;
+            }
+            return "输入组件名字，首字母大写";
+          },
         })
         .then((msg) => {
           util.copyFile(`${__dirname}/tpls`, uri.fsPath, msg ?? "Compontent");
@@ -29,7 +29,23 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(disposable);
+  const genComment = vscode.commands.registerCommand(
+    "gen-compontent-file.genComment",
+    () => {
+      vscode.window.activeTextEditor?.edit((editBuilder) => {
+        //获取光标位置
+        const position = new Position(
+          vscode.window.activeTextEditor?.selection.active.line || 1,
+          vscode.window.activeTextEditor?.selection.active.character || 1
+        );
+        console.log(position);
+        //在光标位置插入字符串
+        editBuilder.insert(position, "width:...");
+      });
+    }
+  );
+  context.subscriptions.push(genComment);
+  context.subscriptions.push(genCompontent);
 }
 
 // this method is called when your extension is deactivated
